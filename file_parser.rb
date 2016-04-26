ff9fbf0ce157e344d2d804055ca214ca8ad98b01
mradambeck <mr.adambeck@gmail.com>
2016-04-26 10:08:58 -0700

    pulling all the data into arrays via CSV method

--- file_parser.rb
+++ file_parser.rb
@@ -1,23 +1,20 @@
-# f = File.open("sample/comma.txt", "r")
-# f.each_line do |line|
-#   puts line
-# end
-# f.close
-#
-# f = File.open("sample/pipe.txt", "r")
-# f.each_line do |line|
-#   puts line
-# end
-# f.close
-#
-# f = File.open("sample/space.txt", "r")
-# f.each_line do |line|
-#   puts line
-# end
-# f.close
-#
 
 require 'csv'
-comma_values = CSV.read('/sample/comma.txt')
-pipe_values = CSV.read('/sample/pipe.txt')
-space_values = CSV.read('/sample/space.txt')
+comma_values = CSV.read('sample/comma.txt')
+pipe_values = CSV.read('sample/pipe.txt', { :col_sep => '|' })
+space_values = CSV.read('sample/space.txt', { :col_sep => ' ' })
+
+p "comma:"
+p comma_values
+
+p "pipe:"
+p pipe_values
+
+p "space"
+p space_values
+
+@people = Hash.new
+
+comma_values.each do |line|
+
+end
