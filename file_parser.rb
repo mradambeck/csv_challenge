f1d340967250ebe9265bda2659d66a91e7bbcedf
mradambeck <mr.adambeck@gmail.com>
2016-04-27 09:45:18 -0700

    date parse broken out

--- file_parser.rb
+++ file_parser.rb
@@ -12,6 +12,17 @@ space_values = CSV.read('sample/space.txt', { :col_sep => ' ' })
 # p "space"
 # p space_values
 
+def date_parse(arr, str)
+  date = arr.strip.split(str)
+  if date[0].length < 2
+    date[0] = '0'+date[0].to_s
+  end
+  if date[1].length < 2
+    date[1] = '0'+date[1].to_s
+  end
+  return date[0]+'/'+date[1]+'/'+date[2]
+end
+
 @people = Array.new
 
 comma_values.each do |line|
@@ -20,10 +31,11 @@ comma_values.each do |line|
   person[:first_name] = line[1].strip
   person[:gender] = line[2].strip
   person[:color] = line[3].strip
-  date = line[4].strip
-  person[:month] = date.split('/')[0]
-  person[:day] = date.split('/')[1]
-  person[:year] = date.split('/')[2]
+  person[:date] = date_parse(line[4],'/')
+  # p person[:date]
+  # person[:month] = date.split('/')[0]
+  # person[:day] = date.split('/')[1]
+  # person[:year] = date.split('/')[2]
   @people << person
 end
 
@@ -38,10 +50,11 @@ pipe_values.each do |line|
     person[:gender] = "Female"
   end
   person[:color] = line[4].strip
-  date = line[5].strip
-  person[:month] = date.split('-')[0]
-  person[:day] = date.split('-')[1]
-  person[:year] = date.split('-')[2]
+  person[:date] = date_parse(line[5],'-')
+  # date = line[5].strip
+  # person[:month] = date.split('-')[0]
+  # person[:day] = date.split('-')[1]
+  # person[:year] = date.split('-')[2]
   @people << person
 end
 
@@ -55,31 +68,32 @@ space_values.each do |line|
   elsif line[3].strip == "F"
     person[:gender] = "Female"
   end
-  date = line[4].strip
-  person[:month] = date.split('-')[0]
-  person[:day] = date.split('-')[1]
-  person[:year] = date.split('-')[2]
+  person[:date] = date_parse(line[4],'-')
+  # date = line[4].strip
+  # person[:month] = date.split('-')[0]
+  # person[:day] = date.split('-')[1]
+  # person[:year] = date.split('-')[2]
   person[:color] = line[5].strip
   @people << person
 end
 
 # puts @people
-
+#
 # Output 1:
 @people.sort_by{ |p| [p[:gender], p[:last_name]] }.each do |person|
-  puts "#{person[:last_name]} #{person[:first_name]} #{person[:gender]} #{person[:month]}/#{person[:day]}/#{person[:year]} #{person[:color]}"
+  puts "#{person[:last_name]} #{person[:first_name]} #{person[:gender]} #{person[:date]} #{person[:color]}"
 end
 
 puts "--------------"
 
 # Output 2:
-@people.sort_by{ |p| p[:year] }.each do |person|
-  puts "#{person[:last_name]} #{person[:first_name]} #{person[:gender]} #{person[:month]}/#{person[:day]}/#{person[:year]} #{person[:color]}"
+@people.sort_by{ |p| p[:date][-4..-1] }.each do |person|
+  puts "#{person[:last_name]} #{person[:first_name]} #{person[:gender]} #{person[:date]} #{person[:color]}"
 end
 
 puts "--------------"
 
 # Output 3:
 @people.sort_by{ |p| p[:last_name] }.reverse.each do |person|
-  puts "#{person[:last_name]} #{person[:first_name]} #{person[:gender]} #{person[:month]}/#{person[:day]}/#{person[:year]} #{person[:color]}"
+  puts "#{person[:last_name]} #{person[:first_name]} #{person[:gender]} #{person[:date]} #{person[:color]}"
 end
