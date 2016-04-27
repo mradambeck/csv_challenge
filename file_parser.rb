242db342a67a60e86612401d266b79820555edca
mradambeck <mr.adambeck@gmail.com>
2016-04-27 09:23:04 -0700

    challenge complete

--- file_parser.rb
+++ file_parser.rb
@@ -6,24 +6,25 @@ space_values = CSV.read('sample/space.txt', { :col_sep => ' ' })
 
 # p "comma:"
 # p comma_values
-#
+
 # p "pipe:"
 # p pipe_values
-#
+
 # p "space"
 # p space_values
 
 @people = Array.new
 
-# @people = Hash.new
-#
 comma_values.each do |line|
   person = Hash.new
-  person[:last_name] = line[0]
-  person[:first_name] = line[1]
-  person[:gender] = line[2]
-  person[:color] = line[3]
-  person[:date] = line[4]
+  person[:last_name] = line[0].strip
+  person[:first_name] = line[1].strip
+  person[:gender] = line[2].strip
+  person[:color] = line[3].strip
+  date = line[4].strip
+  person[:month] = date.split('/')[0]
+  person[:day] = date.split('/')[1]
+  person[:year] = date.split('/')[2]
   @people << person
 end
 
@@ -38,23 +39,48 @@ pipe_values.each do |line|
     person[:gender] = "Female"
   end
   person[:color] = line[4].strip
-  person[:date] = line[5].strip
+  date = line[5].strip
+  person[:month] = date.split('-')[0]
+  person[:day] = date.split('-')[1]
+  person[:year] = date.split('-')[2]
   @people << person
 end
 
 space_values.each do |line|
   person = Hash.new
-  person[:last_name] = line[0]
-  person[:first_name] = line[1]
-  person[:middle_initial] = line[2]
-  if line[3] == "M"
+  person[:last_name] = line[0].strip
+  person[:first_name] = line[1].strip
+  person[:middle_initial] = line[2].strip
+  if line[3].strip == "M"
     person[:gender] = "Male"
-  elsif line[3] == "F"
+  elsif line[3].strip == "F"
     person[:gender] = "Female"
   end
-  person[:date] = line[4]
-  person[:color] = line[5]
+  date = line[4].strip
+  person[:month] = date.split('-')[0]
+  person[:day] = date.split('-')[1]
+  person[:year] = date.split('-')[2]
+  person[:color] = line[5].strip
   @people << person
 end
 
-puts @people
+# puts @people
+
+# Output 1:
+@people.sort_by{ |p| [p[:gender], p[:last_name]] }.each do |person|
+  puts "#{person[:last_name]} #{person[:first_name]} #{person[:gender]} #{person[:month]}/#{person[:day]}/#{person[:year]} #{person[:color]}"
+end
+
+puts "--------------"
+
+# Output 2:
+@people.sort_by{ |p| p[:year] }.each do |person|
+  puts "#{person[:last_name]} #{person[:first_name]} #{person[:gender]} #{person[:month]}/#{person[:day]}/#{person[:year]} #{person[:color]}"
+end
+
+puts "--------------"
+
+# Output 3:
+@people.sort_by{ |p| p[:last_name] }.reverse.each do |person|
+  puts "#{person[:last_name]} #{person[:first_name]} #{person[:gender]} #{person[:month]}/#{person[:day]}/#{person[:year]} #{person[:color]}"
+end
